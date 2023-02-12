0edec4e23a318572f8803d82a70c49fc8f44b537
tsyr <33116430+tsyr@users.noreply.github.com>
2023-02-12 23:02:50 +0800

    Update Draw.py

--- Draw.py
+++ Draw.py
@@ -1,3 +1,4 @@
+# https://juejin.cn/post/7034117434659471397 vector field
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,8 +11,7 @@ f.close()
 
 Z=np.array(Z)
 
-#plt.imshow(Z, extent=[0, 10, 0, 10], origin='lower', cmap='RdGy')
-plt.imshow(Z.T, extent=[0, 20, 0, 10], origin='lower', cmap=plt.cm.jet)
+plt.imshow(Z.T, extent=[0, 10e-2, 0, 5e-2], origin='lower', cmap=plt.cm.jet)
 #plt.imshow(Z, extent=[0, 10, 0, 10], origin='lower', cmap=plt.cm.gray)
 plt.colorbar()
 plt.show()
